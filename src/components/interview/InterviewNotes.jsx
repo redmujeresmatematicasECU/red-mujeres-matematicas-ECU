21db5330d08812b80d0cfc34645d71c238aeacb9
Maria Castellano <maria-jose.castellano-guayasamin.exalt@ext.pmu.fr>
2023-04-07 10:02:02 +0200

    feat(roxana): entrevista

--- src/components/interview/InterviewNotes.jsx
+++ src/components/interview/InterviewNotes.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import mexico from "../../assets/flags/mexico.png";
 import argentina from "../../assets/flags/argentina.png";
 import paisesBajos from "../../assets/flags/paisesBajos.png";
+import peru from "../../assets/flags/peru.png";
 
 const InterviewContainer = styled.ul`
   background: var(--main-red-blue);
@@ -33,6 +34,11 @@ const womanList = [
     name: "María Eugenia Martínez",
     flag: <img src={argentina} alt="argentina" />,
     ref: "https://drive.google.com/file/d/1xIPxHicBsrx797wealGYO27zEN85aOOJ/view?usp=share_link",
+  },
+  {
+    name: "Roxana López",
+    flag: <img src={peru} alt="peru" />,
+    ref: "https://drive.google.com/file/d/1P3Zk1d2gQPB42gvd0DiDoDmiSg1DKTpr/view?usp=sharing",
   },
 ];
 
